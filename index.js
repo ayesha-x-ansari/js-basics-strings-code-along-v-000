dbb4c9a42bffc1a470590db3977a8bd1aa28302b
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-07-25 22:15:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const currentUser = 'Grace Hopper';
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
